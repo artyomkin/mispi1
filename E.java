3ddd2247f35eddd02949a62c61a0618692cdba1e
Artyomkin Artyom <welllet@mail.ru>
2022-04-19 02:46:54 +0300

    r48

--- E.java
+++ E.java
@@ -23,4 +23,8 @@ public class E implements D {
     public String nn() {
         "".>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.;
     }
+
+    public java.util.List<String> jj() {
+        return new java.util.LinkedList<String>();
+    }
 }
