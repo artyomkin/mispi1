fde2d7ab707d4731c62e0d3477ea55cc3821ddb6
Artyomkin Artyom <welllet@mail.ru>
2022-04-19 03:26:47 +0300

    r67

--- G.java
+++ G.java
@@ -31,4 +31,8 @@ public class G {
     public void ab() {
         System.out.println();
     }
+
+    public java.util.Set<Integer> ll() {
+        return new java.util.LinkedList<Integer>;
+    }
 }
